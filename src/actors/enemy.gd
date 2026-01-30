2782614971484600d9a83c7e3400fc51247f18ee
Rupuu <zamyarkov@gmail.com>
2026-01-31 00:56:17 +0200

    progress 2

---
+++ src/actors/enemy.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+#place the player node here
+var player
+
+#func _physics_process(delta: float):
+	#var direction =
+#global_position.direction_to(player.global_position)
+		#velocity = direction * speed_multiplier
+		#move_and_slide()
