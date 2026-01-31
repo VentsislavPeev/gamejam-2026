7714be23ecf357110f14d7271a37954f8a5e6568
Daniel Zamyarkov <62323191+Rupuu@users.noreply.github.com>
2026-01-31 04:27:19 +0200

    Enemy (#5)
    * progressing
    
    * progress 2
    
    * enemy spawn and chase

---
+++ src/actors/enemy.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+var health = 3
+
+@onready var player = get_node("/root/Game/Player")
+
+func _physics_process(delta: float):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 300
+	move_and_slide()
+
+func take_dmg():
+	health -= 1
+	
+	if health == 0:
+		queue_free()
