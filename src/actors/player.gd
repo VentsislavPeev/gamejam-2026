2f75aa3211331de2a4af0c9afb17fb163d97a768
Rupuu <zamyarkov@gmail.com>
2026-01-31 12:33:14 +0200

    taking damage

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 var health = 100
 
-var earth_mask = true;
-var fire_mask = false;
+var earth_mask = false;
+var fire_mask = true;
 var lightning_mask = false;
 
 func _physics_process(delta: float):
@@ -11,9 +11,18 @@ func _physics_process(delta: float):
 	velocity = direction * 600
 	move_and_slide()
 	
-	#if earth_mask:
-		#keep_max_health()
+	const DMG_RATE = 5.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DMG_RATE*overlapping_mobs.size()*delta
+		print('Damage taken')
+		if health <= 0.0:
+			print("DIED")
+	
+	if earth_mask:
+		keep_max_health()
 
 func keep_max_health():
 	if health > 100:
 		health = 100
+		
