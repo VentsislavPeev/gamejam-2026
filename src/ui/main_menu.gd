faca7dfa2f49035059d3dc821ae4034bd8b73a33
vixen143 <myrrh852@gmail.com>
2026-01-31 21:04:49 +0200

    Marti UI (#9)
    * main menu added
    
    * Start Panel, Pause Menu(not working properly), Game Over Menu (not working properly)
    
    * added audio and ui extras
    
    * 1
    
    * SFX - shooting, enemy kill, enemy burning, theme music, idle menu sound
    
    * audio fix
    
    * neshto
    
    * fixed

---
+++ src/ui/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+#@onready var ambience_loop = $AudioStreamPlayer
+@onready var start_button: Button = $StartButton
+
+func _ready() -> void:
+	start_button.pressed.connect(_on_start_pressed)
+
+func _on_start_pressed() -> void:
+	#ambience_loop.stop()
+	get_tree().change_scene_to_file("res://src/levels/survivor_game.tscn")
