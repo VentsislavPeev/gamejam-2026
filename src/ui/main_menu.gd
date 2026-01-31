5e04c5ea6722cda4ff0285e0ae096230cc88fb3a
vixen143 <myrrh852@gmail.com>
2026-01-31 19:44:37 +0200

    audio fix

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -1,12 +1,11 @@
 extends Control
 
-@onready var ambience_loop = $AudioStreamPlayer
+#@onready var ambience_loop = $AudioStreamPlayer
 @onready var start_button: Button = $StartButton
 
 func _ready() -> void:
-	ambience_loop.play()
 	start_button.pressed.connect(_on_start_pressed)
 
 func _on_start_pressed() -> void:
-	ambience_loop.stop()
+	#ambience_loop.stop()
 	get_tree().change_scene_to_file("res://src/levels/survivor_game.tscn")
