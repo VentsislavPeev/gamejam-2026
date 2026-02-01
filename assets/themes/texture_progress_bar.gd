c6ba7bb6556c0da974c26352571618ba1f8a47c5
Venci <vencislavpeev18.8@gmail.com>
2026-02-01 14:16:04 +0200

    good

--- assets/themes/texture_progress_bar.gd
+++ assets/themes/texture_progress_bar.gd
@@ -3,14 +3,19 @@ extends TextureProgressBar
 # Use %Player if you set "Access as Unique Name" on the Player node
 # Otherwise, keep your get_node path if it works
 @onready var player = get_node("/root/Game/Player")
+@onready var level_label = $LevelText # Reference the node properly
 var level = 1
 var new_level_exp = 0
 var last_level_exp = 0
 
+
 func _ready():
 	# 1. Initialize the bar immediately
+	level = player.level
 	value = player.experience
 	max_value = player.calculate_experience_to_level(level)
+	
+	update_level_display()
 	
 	# 2. Connect the signal to a specific function
 	# The signal emits (new_health), so our function must accept (new_health)
@@ -23,6 +28,10 @@ func _on_exp_change(new_exp):
 		last_level_exp = player.calculate_experience_to_level(level)
 		max_value = new_level_exp
 		level = player.level
+		update_level_display()
 		new_level_exp = player.calculate_experience_to_level(level) - last_level_exp
 	max_value = player.calculate_experience_to_level(level) - last_level_exp
 	value = new_exp - last_level_exp
+	
+func update_level_display():
+	level_label.text = "[font_size=36]LVL"+str(level)+"[/font_size]"
